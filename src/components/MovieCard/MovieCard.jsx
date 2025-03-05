ad3c3248dffa7349b5d70a03e96e624dee55cb10
Oleg Orykhivskiy <olegkoolll@gmail.com>
2025-03-05 16:46:02 +0200

    done3

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,27 +1,31 @@
 import './MovieCard.scss';
 
-export const MovieCard = ({ props }) => (
-  <div className="card-content">
-    <div className="media">
-      <div className="media-left">
-        <figure className="image is-48x48">
-          <img src={props.imgUrl} alt="imdb" />
-        </figure>
+export const MovieCard = ({ props }) => {
+  const { imgUrl, title, description, imdbUrl } = props;
+
+  return (
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src={imgUrl} alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8" data-cy="MovieTitle">
+            {title}
+          </p>
+        </div>
       </div>
 
-      <div className="media-content">
-        <p className="title is-8" data-cy="MovieTitle">
-          {props.title}
-        </p>
+      <div className="content">
+        <p data-cy="MovieDescription">{description}</p>
+
+        <a href={imdbUrl} data-cy="MovieLink">
+          IMDB
+        </a>
       </div>
     </div>
-
-    <div className="content">
-      <p data-cy="MovieDescription">{props.description}</p>
-
-      <a href={props.imdbUrl} data-cy="MovieLink">
-        IMDB
-      </a>
-    </div>
-  </div>
-);
+  );
+};
