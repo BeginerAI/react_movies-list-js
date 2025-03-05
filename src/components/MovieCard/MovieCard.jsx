448ccb61ce9c2e291235b0ed14ed0996a033a5cc
Oleg Orykhivskiy <olegkoolll@gmail.com>
2025-03-05 16:33:32 +0200

    done1

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,3 +1,27 @@
 import './MovieCard.scss';
 
-export const MovieCard = () => <>Put the card here</>;
+export const MovieCard = ({ props }) => (
+  <div className="card-content">
+    <div className="media">
+      <div className="media-left">
+        <figure className="image is-48x48">
+          <img src={props.imgUrl} alt="imdb" />
+        </figure>
+      </div>
+
+      <div className="media-content">
+        <p className="title is-8" data-cy="MovieTitle">
+          {props.title}
+        </p>
+      </div>
+    </div>
+
+    <div className="content">
+      <p data-cy="MovieDescription">{props.description}</p>
+
+      <a href={props.imdbUrl} data-cy="MovieLink">
+        IMDB
+      </a>
+    </div>
+  </div>
+);
